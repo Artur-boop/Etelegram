b396c0101b7d6ee567eca6151f91b3782b07b11d
artur_bertash <77884793+Artur-boop@users.noreply.github.com>
2024-01-28 15:49:45 +0200

    Update main.py
    sunday issue

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from datetime import date
 import time
 from datetime import datetime
 from datetime import datetime
+from datetime import datetime, timedelta
 
 
-
-bot = telebot.TeleBot('###')
+bot = telebot.TeleBot('6487252741:AAFOYo2PBeFXoBNOP2aNfQD7aT03C0mVBSk')
 
 
 
@@ -16,7 +16,7 @@ stickers_data = {
         1: f"8:50 - Укр мова\n10:35 - Алгебра \n12:30 - Інформатика \n14:15 - немає",
         2: f"8:50 - Всесвітня\n10:35 - Фізика \n12:30 - Астрономія \n14:15 - Технології",
         3: f"8:50 - Геометрія\n10:35 - Англ мова \n12:30 - Алгебра \n14:15 - ШПД Фізика",
-        4: f"8:50 - Захист \n10:35 - Фізра \n12:30 - Біологія \n14:15 - ШПД Укр мова ",
+        4: f"8:50 - Захист \n10:35 - Фізра \n12:30 - Біологія \n14:15 - ШПД Математика",
         5: f"8:50 - Фізика \n10:35 - Англ \n12:30 - Алгебра \n14:15 - Укр літ "
     },
     2: {
@@ -42,14 +42,14 @@ stickers_data = {
     }
 }
 
-time_data = [['08:50:00', '09:35:00'], ['09:35:00', '09:40:00'], ['09:40:00', '10:25:00'], 
-                ['10:25:00', '10:35:00'], 
+time_data = [['08:50:00', '09:35:00'], ['09:35:00', '09:40:00'], ['09:40:00', '10:25:00'],
+                ['10:25:00', '10:35:00'],
                 ['10:35:00', '11:20:00'], ['11:20:00', '11:25:00'], ['11:25:00', '12:10:00'],
-                ['12:10:00', '12:30:00'], 
-                ['12:30:00', '13:15:00'], ['13:15:00', '13:20:00'], ['13:20:00', '14:00:00'], 
-                ['14:00:00', '14:15:00'], 
+                ['12:10:00', '12:30:00'],
+                ['12:30:00', '13:15:00'], ['13:15:00', '13:20:00'], ['13:20:00', '14:00:00'],
+                ['14:00:00', '14:15:00'],
                 ['14:15:00', '15:00:00'], ['15:00:00', '15:05:00'], ['15:05:00', '15:50:00']]
-                
+
 time_names = ['перша пара перша півпара', 'маленька перерва', "перша пара друга півпара",
             'перша велика пара',
             'друга пара перша півпара', 'маленька перерва', 'друга пара друга півпара',
@@ -78,21 +78,21 @@ def calc_difference(start_date, cur_date):
     return days_passed + 1
 
 def calc_week(num_days):
-    
+
     if num_days <= 7:
         return(1, num_days)
     else:
-        weeks = num_days // 7 
+        weeks = num_days // 7
         days = num_days % 7
-        
+
         if days == 0 and weeks >= 1:
             if weeks <= 4:
                 return(weeks,days)
-            else: 
+            else:
                 return (weeks % 4)
         elif days != 0 and weeks >= 1:
             return (weeks + 1, days)
-        
+
 
 @bot.message_handler(commands=['diary_today'])
 def diary_sender1(message):
@@ -123,15 +123,15 @@ def diary_sender1(message):
             message_txt += stickers_data[int(week)][int(day)]
     response = message_txt + '\n\n' + '\N{watch}'
     bot.send_message(message.chat.id, response)
- 
+
 @bot.message_handler(commands=['diary_tomorrow'])
 def diary_sender2(message):
     today = date.today()
-    start_date = '2024-01-22'  
+    start_date = '2024-01-22'
     cur_date = str(today)
     message_txt = ''
     print(cur_date)
-    if cur_date == '2024-01-21':  
+    if cur_date == '2024-01-21':
         print('hey')
         message_txt += ('Завтра - ПОНЕДІЛОК' + '\N{skull}.' + 'Буде такий розклад:')
         message_txt += '\n' + stickers_data[1][1]
@@ -143,7 +143,7 @@ def diary_sender2(message):
         day = str((calc_week(num_days)))[-2]
         print('week - ', int(week))
         print('day - ', int(day))
-        if int(day) == 0:
+        if int(day) == 7:
             message_txt += ('Завтра - ПОНЕДІЛОК' + '\N{skull}.' + 'Буде такий розклад:')
             message_txt += '\n' +stickers_data[int(week) + 1][1]
         elif int(day) == 6:
@@ -158,7 +158,10 @@ def diary_sender2(message):
     response = message_txt + '\n\n' + '\N{watch}'
     bot.send_message(message.chat.id, response)
 
-
+# 8.50 - 9.35, 9.35-9.40, 9.40-10.25, 10.25-10.35
+#
+#
+#
 
 
 def is_within_boundaries(time1_str, time2_str, current_time_str):
@@ -174,7 +177,7 @@ def is_within_boundaries(time1_str, time2_str, current_time_str):
         return True
     else:
         return False
-        
+
 def time_difference(input1, input2):
     # Convert string inputs to datetime objects
     time_format = "%H:%M:%S"
@@ -201,37 +204,43 @@ def time_difference(input1, input2):
 
 @bot.message_handler(commands=['time'])
 def diary_sender2(message):
-    
-    current_time = datetime.now().strftime("%H:%M:%S")
-    
-    if is_within_boundaries('08:50:00', '15:50:00', current_time) == False:
+
+    current_datetime = datetime.now()
+
+    # Subtract two hours from the current datetime
+    result_datetime = current_datetime + timedelta(hours=2)
+
+    # Format the result as a string
+    result_time = result_datetime.strftime('%H:%M:%S')
+    #bot.reply_to(message, result_time )
+    if is_within_boundaries('08:50:00', '15:50:00', result_time) == False:
         message_txt = 'Броускі чіл. Зараз НЕМАЄ пар'
     for i in range(len(time_data)):
-        if is_within_boundaries(time_data[i][0], time_data[i][1], str(current_time)):
+        if is_within_boundaries(time_data[i][0], time_data[i][1], str(result_time)):
            if 'перерва' in time_names[i].lower():
-               
-                message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна півпара/пара буде через - ' + str(time_difference(current_time, time_data[i][1]))
+
+                message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна півпара/пара буде через - ' + str(time_difference(result_time, time_data[i][1]))
            elif 'пара' in time_names[i].lower():
                 if 'четверта' not in time_names[i] and 'друга' not in time_names[i] and 'маленька' in time_names[i + 1].lower():
-                  
-                    message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна маленька перерва буде через - ' + str(time_difference(current_time, time_data[i][1])) 
-                    
+
+                    message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна маленька перерва буде через - ' + str(time_difference(result_time, time_data[i][1]))
+
                 elif 'четверта' not in time_names[i] and 'друга' not in time_names[i] and 'велика' in time_names[i + 1].lower():
-                    message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна велика перерва буде через - ' + str(time_difference(current_time, time_data[i][1])) 
+                    message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Наступна велика перерва буде через - ' + str(time_difference(result_time, time_data[i][1]))
                 else:
-                      message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Закінчення пари через - ' + str(time_difference(current_time, time_data[i][1]))
-            
+                      message_txt = 'Зараз - '+ str(time_names[i]) + '\n' + 'Закінчення пари через - ' + str(time_difference(result_time, time_data[i][1]))
+
     output = message_txt + '\n\n' + '\N{watch}'
-    bot.reply_to(message, output )                 
-    
+    bot.reply_to(message, output )
+
 @bot.message_handler(commands=['week'])
 def diary_sender5(message):
     today = date.today()
-    start_date = '2024-01-22'  
+    start_date = '2024-01-22'
     cur_date = str(today)
     message_txt = ''
     print(cur_date)
-    if cur_date == '2024-01-21':  
+    if cur_date == '2024-01-21':
         print('hey')
         message_txt += ('Завтра - ПОНЕДІЛОК' + '\N{skull}.' + 'Буде такий розклад:')
         message_txt += '\n' + stickers_data[1][1]
@@ -267,21 +276,21 @@ def diary_sender5(message):
             message_txt += '\n' + 'Пятниця' + '\n' +stickers_data[int(week) + 1][5] + '\n'
         else:
             message_txt += 'Тиждень - ' +  str(week) + '. Протягом тижня ось такий розклад:'
-            
-            
+
+
 
             message_txt += '\n' + 'Понеділок' + '\n' +stickers_data[int(week) ][1] + '\n'
             message_txt += '\n' + 'Вівторок' + '\n' +stickers_data[int(week) ][2] + '\n'
             message_txt += '\n' + 'Середа' + '\n' +stickers_data[int(week) ][3] + '\n'
             message_txt += '\n' + 'Четвер' + '\n' +stickers_data[int(week) ][4] + '\n'
             message_txt += '\n' + 'Пятниця' + '\n' +stickers_data[int(week) ][5] + '\n'
-        
+
 
 
     response = message_txt + '\n\n' + '\N{watch}'
     bot.send_message(message.chat.id, response)
 
-    
-    
-    
+
+
+
 bot.infinity_polling(none_stop=True)
